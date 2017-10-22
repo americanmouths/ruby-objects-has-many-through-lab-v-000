081c688aa12c680f13423905dde37860d4a3328f
Kayla Hansen <shiverr@gmail.com>
2017-10-22 22:52:40 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,9 +1,14 @@
-class Doctor 
+class Doctor
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @appointments = []
   end
-  
+
+  def add_appointment(new_appointment)
+    @appointments << new_appointment
+    new_appointment.doctor = self
+  end
+
 end
